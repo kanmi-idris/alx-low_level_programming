ecda2df78e2aef52dd247a139bc849d331c344a1
olasunkanmi <olasunkanmiidris15@gmail.com>
2022-09-21 13:35:09 +0100

    Done: 5-sign task

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *print_sign - checks for alphabetic character
+ * @c: the character to check
+ * Return: 1 if c is lowercase and 0 if otherwise
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+} else if (n == 0)
+{
+  _putchar('0');
+  return (0);
+} else if (n < 0)
+{
+  _putchar('-');
+  return (-1);
+}
+{
+
+}
+}
