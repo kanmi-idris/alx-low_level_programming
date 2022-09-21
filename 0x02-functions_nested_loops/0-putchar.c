c65bfddc47e669150560663c5854632a07cbb3f4
olasunkanmi <olasunkanmiidris15@gmail.com>
2022-09-21 10:49:14 +0100

    0._putchar Task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
- * main - prints "_putchar" followed by a new line
- * Return: The program returns 0
- */ 
-int main(void){
+* main - prints "_putchar" followed by a new line
+* Return: The program returns 0
+*/ 
+int main(void)
+{
 _putchar('_');
 _putchar('p');
 _putchar('u');
