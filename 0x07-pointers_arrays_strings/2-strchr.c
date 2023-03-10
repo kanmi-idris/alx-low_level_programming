df8170f7301ead51219709fc7bc6e06d6666d573
olasunkanmi <olasunkanmiidris15@gmail.com>
2023-03-10 23:11:13 +0100

    2-strc

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -17,8 +17,8 @@ char *_strchr(char *s, char c)
         {
                 if (c == s[i])
                 {
-                        return (&s[i]);
-                }
+					return (&s[i]);
+				}
         }
         return (NULL);
 }
