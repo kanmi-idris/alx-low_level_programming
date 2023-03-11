e1695fb0b562092662f1a43ad4258a80ea1ae925
olasunkanmi <olasunkanmiidris15@gmail.com>
2023-03-11 18:49:01 +0100

    finished: 3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,7 +1,7 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
-* _strlen_recursion - returns the factorial of a given number.
+* factorial - returns the factorial of a given number.
 * @n: integer number
 * Return: factorial of a given number.
 */
@@ -15,20 +15,5 @@ if (n == 0)
 {
 return (1);
 }
-return (_strlen_recursion(n * (n - 1)));
-}
-
-int main(void)
-{
-    int r;
-
-    r = factorial(1);
-    printf("%d\n", r);
-    r = factorial(5);
-    printf("%d\n", r);
-    r = factorial(10);
-    printf("%d\n", r);
-    r = factorial(-1024);
-    printf("%d\n", r);
-    return (0);
+return ( n * factorial(n - 1));
 }
