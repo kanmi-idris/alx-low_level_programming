83ace25a60ab362946b46ae41739191d9604b17e
olasunkanmi <olasunkanmiidris15@gmail.com>
2023-03-11 17:49:16 +0100

    finished: 2-strlen_recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _print_rev_recursion - prints a string in reverse.
+ * @s: string to be printed
+ */
+int _strlen_recursion(char *s)
+{
+    if (*s == '\0')
+	{
+		return 0;
+	}
+
+    return 1 + _strlen_recursion(s + 1);
+}
