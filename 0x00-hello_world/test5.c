43979b8825e6ab14d0419afc82ba2c6a072b0b80
olasunkanmi <olasunkanmiidris15@gmail.com>
2023-03-14 18:56:04 +0100

    Complete: static libraries

--- 0x00-hello_world/test5.c
+++ 0x00-hello_world/test5.c
@@ -4,8 +4,20 @@
  * main - prints the phrase "Programming is like building a multilingual puzzle
  * Return: 0 if exited properly, non-zero otherwise
  */
+
+void print(int nb)
+{
+    if (nb < 0) 
+    {
+        return;
+    }
+    printf("%d", nb);
+    nb --;
+    print(nb);
+}
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+    print(4);
+    return (0);
 }
