3123ce16ff6de95018fc20f2ac1f17b7787ccd98
olasunkanmi <olasunkanmiidris15@gmail.com>
2023-03-15 12:38:48 +0100

    fixed incorrect output

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,24 +12,22 @@
  */
 int main(int argc, char *argv[])
 {
-    int sum = 0;
-    int i, j;
+int sum = 0;
+int i, j;
+if (argc == 1) {
+printf("0\n");
+return (0);
+}
+for (i = 1; i < argc; i++) {
+for (j = 0; argv[i][j]; j++) {
+if (!isdigit(argv[i][j])) {
+printf("Error\n");
+return 1;
+}
+}
+sum += atoi(argv[i]);
+}
+printf("%d\n", sum);
 
-    if (argc == 1) {
-        printf("0\n");
-        return 0;
-    }
-
-    for (i = 1; i < argc; i++) {
-        for (j = 0; argv[i][j]; j++) {
-            if (!isdigit(argv[i][j])) {
-                printf("Error\n");
-                return 1;
-            }
-        }
-        sum += atoi(argv[i]);
-    }
-
-    printf("%d\n", sum);
-    return 0;
+return (0);
 }
