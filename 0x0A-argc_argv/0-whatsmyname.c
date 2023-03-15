c45f7bccabb3be912288cef6c502b2efc8c13a14
olasunkanmi <olasunkanmiidris15@gmail.com>
2023-03-15 09:50:04 +0100

    FIX: removed trailing whitespaces

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,12 +5,12 @@
  * main - a program that prints its name, followed by a new line.
  * @argc: number of arguments passed.
  * @argv: array of arguments passed.
- * 
+ *
  * Return - name of the program
 */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-    printf("%s\n", argv[0]);
+printf("%s\n", argv[0]);
 
-    return (0);
+return (0);
 }
