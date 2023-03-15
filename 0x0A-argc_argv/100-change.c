5d9fa3d9253ba6b4cbfcee07ea02b78df1afc029
olasunkanmi <olasunkanmiidris15@gmail.com>
2023-03-15 13:18:20 +0100

    fix: compilaion error

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -11,18 +11,18 @@
  */
 int main(int argc, char *argv[])
 {
+int cents, num_coins = 0;
 if (argc != 2)
 {
 printf("Error\n");
 return (1);
 }
-int cents = atoi(argv[1]);
+cents = atoi(argv[1]);
 if (cents < 0)
 {
 printf("0\n");
 return (0);
 }
-int num_coins = 0;
 num_coins += cents / 25;
 cents %= 25;
 num_coins += cents / 10;
