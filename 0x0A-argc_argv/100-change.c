34d3e445750b8589dc449c1f0b197e8a36ff1fca
olasunkanmi <olasunkanmiidris15@gmail.com>
2023-03-15 13:21:32 +0100

    fix: 100-change.c:6: WARNING: line over 80 characters (84)

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 /**
- * main -  prints the minimum number of coins to make change for an amount of money
+ * main -  prints minimum n0 of coins to make change for an amount of money
  * @argc: number of arguments passed.
  * @argv: array of arguments passed.
  *
