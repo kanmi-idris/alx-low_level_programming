f512fce96f9e5c55932235e543f2eda32401da39
olasunkanmi <olasunkanmiidris15@gmail.com>
2023-03-15 12:43:15 +0100

    fixed: betty error of brace on next line

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,13 +14,18 @@ int main(int argc, char *argv[])
 {
 int sum = 0;
 int i, j;
-if (argc == 1) {
+
+if (argc == 1)
+{
 printf("0\n");
 return (0);
 }
-for (i = 1; i < argc; i++) {
-for (j = 0; argv[i][j]; j++) {
-if (!isdigit(argv[i][j])) {
+for (i = 1; i < argc; i++)
+{
+for (j = 0; argv[i][j]; j++)
+{
+if (!isdigit(argv[i][j]))
+{
 printf("Error\n");
 return 1;
 }
