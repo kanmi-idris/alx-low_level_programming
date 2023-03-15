235e8f6abd040565978e1cc0c70ad8f5eeea0e13
olasunkanmi <olasunkanmiidris15@gmail.com>
2023-03-15 09:40:58 +0100

    print file name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - a program that prints its name, followed by a new line.
+ * @argc: number of arguments passed.
+ * @argv: array of arguments passed.
+ * 
+ * Return - name of the program
+*/
+int main(int argc, char* argv[])
+{
+    printf("%s\n", argv[0]);
+
+    return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,7 @@
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
+int _putchar(char c);
+int print_program_name(int argc, char *argv[]);
+
+#endif
