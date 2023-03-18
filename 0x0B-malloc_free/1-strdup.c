510d19d4fb72fff7b253be16aeb1918ed11b1518
olasunkanmi <olasunkanmiidris15@gmail.com>
2023-03-18 13:35:11 +0100

    Complete: strdup program clone complete

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup() returns a pointer to a space in memory.
+ * @str: string
+ *
+ * Return: pointer to a newly allocated space in memory
+ */
+char *_strdup(char *str)
+{
+char *nstr;
+int i = 0, len = 0;
+if (str == NULL)
+{
+return (NULL);
+}
+while (str[len] != '\0')
+{
+len++;
+}
+    
+nstr = malloc(sizeof(char) * len);
+
+if (nstr == NULL)
+{
+return (NULL);
+}
+while (i < len)
+{
+nstr[i] = str[i];
+i++;
+}
+    
+return (nstr);
+}
