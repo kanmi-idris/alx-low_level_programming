4e07e28040fc7ae3fd91e63e790236e8fe4e6b5a
olasunkanmi <olasunkanmiidris15@gmail.com>
2023-03-18 16:10:03 +0100

    COMPLETE: string concatenation

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,47 +11,36 @@
 char *str_concat(char *s1, char *s2)
 {
     char *store;
-    int i = 0, lenS1 = 0, lenS2 = 0, total_len;
-
-
+    int i = 0, j = 0, total_len;
     if (s1 == NULL)
     {
         s1 = "";
     }
-
     if (s2 == NULL)
     {
         s2 = "";
     }
-
-    while (s1[lenS1] != '\0')
+    while (s1[i] || s2[i])
     {
-        lenS1++;
+        i++;
     }
-
-    while (s2[lenS2] != '\0')
-    {
-        lenS2++;
-    }
-
-    total_len = lenS1 + lenS2;
+    total_len = i;
     store = malloc(sizeof(char) * (total_len + 1));
     if (store == NULL)
     {
         return (NULL);
     }
-
-    while (i < (lenS1))
+    while (s1[i])
     {
         store[i] = s1[i];
         i++;
     }
-    while (i < (lenS2))
+    while (s2[j])
     {
-        store[i + lenS1] = s2[i];
+        store[i] = s2[j];
         i++;
+        j++;
     }
-
     store[total_len] = '\0';
 
     return (store);
