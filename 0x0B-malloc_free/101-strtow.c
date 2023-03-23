3c5ad701024d6cc50d1496a181b1fe1ca7f0386d
olasunkanmi <olasunkanmiidris15@gmail.com>
2023-03-23 07:47:38 +0100

    Error Fix: that open brace { should be on the next line

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -13,21 +13,24 @@ int i, j, len, num_words = 0;
 char **words;
 if (str == NULL || *str == '\0')
 return (NULL);
-for (i = 0; str[i]; i++) {
+for (i = 0; str[i]; i++)
+{
 if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
 num_words++;
 }
 words = malloc(sizeof(char *) * (num_words + 1));
 if (words == NULL)
 return (NULL);
-for (i = 0, j = 0; j < num_words; j++) {
+for (i = 0, j = 0; j < num_words; j++)
+{
 while (*str == ' ')
 str++;
 len = 0;
 while (str[len] != ' ' && str[len] != '\0')
 len++;
 words[j] = malloc(sizeof(char) * (len + 1));
-if (words[j] == NULL) {
+if (words[j] == NULL)
+{
 for (i = 0; i < j; i++)
 free(words[i]);
 free(words);
