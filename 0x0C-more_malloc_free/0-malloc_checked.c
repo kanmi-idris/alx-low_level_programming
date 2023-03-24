fe7e794da2b8b4f3cca24f25c8ff453d0e8f820d
olasunkanmi <olasunkanmiidris15@gmail.com>
2023-03-24 16:29:31 +0100

    FIX: Betty Error

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,21 +1,21 @@
-#include "main.h"
 #include <stdlib.h>
+
+#include "main.h"
 
 /**
  * malloc_checked -  allocates memory using malloc
  * @b: integer argument
- * 
- * Return - Pointer to allocated memory
-*/
+ *
+ * Return: pointer to allocated memory
+ */
 
-void *malloc_checked(unsigned int b)
-{
-    void *memory;
-    memory = malloc(sizeof(unsigned int) * b);
-    if (memory == NULL)
-    {
-        exit(98);
-    }
+void *malloc_checked(unsigned int b) {
+        void *memory;
 
-    return (memory);
+        memory = malloc(sizeof(unsigned int) * b);
+        if (memory == NULL) {
+                exit(98);
+        }
+
+        return (memory);
 }
