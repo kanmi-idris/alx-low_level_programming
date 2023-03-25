98e198d8f0167454538df3dc0b54c7acd13dcd92
kanmi-idris <olasunkanmiidris15@gmail.com>
2023-03-25 11:51:00 +0100

    fixed betty errors

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,34 +2,34 @@
 #include <stdlib.h>
 
 /**
-*calloc - function that concatenates two strings
-*@nmemb: amount of elements to be stored in array
-*@size: size of each element stored in the array
-*
-*Return: pointer to the allocated memory
-*/
+ *_calloc - function that concatenates two strings
+ *@nmemb: amount of elements to be stored in array
+ *@size: size of each element stored in the array
+ *
+ *Return: pointer to the allocated memory
+ */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *array;
- unsigned int i;
+char *array;
+unsigned int i;
 
-	if (nmemb == 0 || size == 0)
-	{
-		return (NULL);
- }
+if (nmemb == 0 || size == 0)
+{
+return (NULL);
+}
 
- array = malloc(nmemb * size);
-	if (!array)
-	{
-		return (NULL);
- }
+array = malloc(nmemb * size);
+if (!array)
+{
+return (NULL);
+}
 
- i = 0;
-	while (i < nmemb * size)
-	{
-  array[i] = 0;
-		i++;
- }
+i = 0;
+while (i < nmemb * size)
+{
+array[i] = 0;
+i++;
+}
 
- return (array);
+return (array);
 }
