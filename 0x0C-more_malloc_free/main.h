e851ea4f5e449e8fda15feac9fee6ca109741f50
kanmi-idris <olasunkanmiidris15@gmail.com>
2023-03-25 09:15:06 +0100

    ADD: added putchar prototype to main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -10,7 +10,7 @@
  * 
  * Putting these three lines of code together creates a "header guard" that helps prevent multiple inclusions of the same header file
 */
-
+int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
