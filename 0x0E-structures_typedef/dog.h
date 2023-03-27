5bbe7818dd459ceb8de91a511721be65a9dd1d63
kanmi-idris <olasunkanmiidris15@gmail.com>
2023-03-27 23:06:48 +0100

    added a readme.md file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,6 +16,7 @@ float age;
 char *owner;
 } dog_t;
 
+int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
