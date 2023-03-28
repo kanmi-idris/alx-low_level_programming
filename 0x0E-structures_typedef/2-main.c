c01740e5a3a335ff4d086c166e074a315c008968
kanmi-idris <olasunkanmiidris15@gmail.com>
2023-03-28 09:17:53 +0100

    FIX: incorrect output for test cases print_dog(&my_dog); where my_dog contains NULL pointers for name and owner &&& print_dog(NULL);

--- 0x0E-structures_typedef/2-main.c
+++ 0x0E-structures_typedef/2-main.c
@@ -1,18 +1,14 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include "dog.h"
 
 /**
- * main - check the code
+ * main - check the code .
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    struct dog my_dog;
-
-    my_dog.name = "Poppy";
-    my_dog.age = 3.5;
-    my_dog.owner = "Bob";
-    print_dog(&my_dog);
-    return (0);
+	print_dog(NULL);
+	return (0);
 }
--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,25 +11,31 @@
 
 void print_dog(struct dog *d)
 {
-if (d->name == NULL)
-{
-printf("Name: (nil)");
-}
-else if (d->owner == NULL)
-{
-printf("Owner: (nil)");
-}
-else if (!d->age)
-{
-printf("Age: (nil)");
-}
-
-printf("Name: %s\n", d->name);
-printf("Age: %f\n", d->age);
-printf("Owner: %s\n", d->owner);
 
 if (d == NULL)
 {
 printf("\n");
+return;
 }
+
+if (d->name == NULL)
+{
+printf("Name: (nil)\n");
+}
+else 
+{
+printf("Name: %s\n", d->name);
+}
+
+printf("Age: %f\n", d->age);
+
+if (d->owner == NULL)
+{
+printf("Owner: (nil)\n");
+}
+else
+{
+printf("Owner: %s\n", d->owner);
+}
+
 }
