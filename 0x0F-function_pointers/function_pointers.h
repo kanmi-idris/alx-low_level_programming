f95f32c123f56e2676a085ed29e855c69ae03c2e
kanmi-idris <olasunkanmiidris15@gmail.com>
2023-04-12 23:45:22 +0100

    fix: betheaderguard not properly placed

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,8 @@
-#include <stddef.h>
 #ifndef FUNC_PTR
 #define FUNC_PTR
+#include <stddef.h>
 
+int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
