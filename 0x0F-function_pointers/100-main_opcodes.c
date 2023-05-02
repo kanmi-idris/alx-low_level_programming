38a9fae8dd0eac142c4d2c53b69df39e52e902b0
olaidris <olasunkanmiidris15@gmail.com>
2023-05-02 07:03:15 +0100

    task 4 completed

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the opcodes of its own main function
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 on success, 1 if argc is not the correct one,
+ * 2 if number of bytes is negative
+ */
+int main(int argc, char *argv[])
+{
+    int i, n;
+    char *p;
+
+    if (argc != 2)
+    {
+        printf("Error\n");
+        exit(1);
+    }
+    n = atoi(argv[1]);
+    if (n < 0)
+    {
+        printf("Error\n");
+        exit(2);
+    }
+    p = (char *)main;
+    for (i = 0; i < n; i++)
+    {
+        printf("%02hhx", p[i]);
+        if (i < n - 1)
+            printf(" ");
+    }
+    printf("\n");
+    return (0);
+}
