69d59d244bba3fc75a70b6b4ceca5186a255e181
olaidris <olasunkanmiidris15@gmail.com>
2023-05-02 07:04:38 +0100

    implemented betty formatting

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -11,27 +11,27 @@
  */
 int main(int argc, char *argv[])
 {
-    int i, n;
-    char *p;
+int i, n;
+char *p;
 
-    if (argc != 2)
-    {
-        printf("Error\n");
-        exit(1);
-    }
-    n = atoi(argv[1]);
-    if (n < 0)
-    {
-        printf("Error\n");
-        exit(2);
-    }
-    p = (char *)main;
-    for (i = 0; i < n; i++)
-    {
-        printf("%02hhx", p[i]);
-        if (i < n - 1)
-            printf(" ");
-    }
-    printf("\n");
-    return (0);
+if (argc != 2)
+{
+printf("Error\n");
+exit(1);
+}
+n = atoi(argv[1]);
+if (n < 0)
+{
+printf("Error\n");
+exit(2);
+}
+p = (char *)main;
+for (i = 0; i < n; i++)
+{
+printf("%02hhx", p[i]);
+if (i < n - 1)
+printf(" ");
+}
+printf("\n");
+return (0);
 }
