f58bca17c8a9bb3c191d3b044fe45076f158e773
olaidris <olasunkanmiidris15@gmail.com>
2023-05-03 02:46:07 +0100

    task 2 of alx variadic functions completed

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - prints numbers, followed by a new line.
+ * @n: no of integers to be passed
+ * @separator: string to be printed between numbers
+ *
+ * Return: 0 (successful)
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+unsigned int i = 0;
+va_list(print_nums);
+va_start(print_nums, n);
+
+while (i < n)
+{
+printf("%d", va_arg(print_nums, int));
+if (separator != NULL)
+printf("%s", separator);
+
+i++;
+}
+printf("\n");
+va_end(print_nums);
+}
