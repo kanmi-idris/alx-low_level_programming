8339b26878daadd723e45a03881837fb23628bce
olaidris <olasunkanmiidris15@gmail.com>
2023-05-03 21:23:18 +0100

    fix: betty error no description found for the premain function.

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * premain - prints a message before main function is executed
+ *
+ * Return: void
+ */
 void __attribute__((constructor)) premain()
 {
 printf("You're beat! and yet, you must allow,\n");
