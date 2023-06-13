4bf3e4997d95c2895d148c67edc6a8a27f3e8c1d
olaidris <olasunkanmiidris15@gmail.com>
2023-06-13 23:08:11 +0100

    fix the over 80 charater limit

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# This line is a shebang that tells the operating system to use bash as the interpreter for this script
+# A shebang that tells the OS to use bash as the interpreter for this script
 
 gcc -c *.c
-# This line uses the gcc command to compile all the .c files in the current directory into object files with the -c flag
+# Compile all .c files in current dir into object files with gcc -c
 
 ar -rc liball.a *.c
-# This line uses the ar command to create a static library called liball.a from all the .c files in the current directory with the -rc flags
+# Create liball.a static library from all .c files in current dir with ar -rc
