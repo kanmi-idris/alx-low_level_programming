6ccc088fc772c4dbddbb5864428d5403e8756033
olaidris <olasunkanmiidris15@gmail.com>
2023-06-13 23:02:42 +0100

    fixed dos line endings error in liball file

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+# This line is a shebang that tells the operating system to use bash as the interpreter for this script
+
 gcc -c *.c
-ar rc liball.a *.o
+# This line uses the gcc command to compile all the .c files in the current directory into object files with the -c flag
+
+ar -rc liball.a *.c
+# This line uses the ar command to create a static library called liball.a from all the .c files in the current directory with the -rc flags
