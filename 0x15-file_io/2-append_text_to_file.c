96cda116fb1c1d7014c6df4849698cd6b30c7d29
olaidris <olasunkanmiidris15@gmail.com>
2023-07-23 22:29:15 +0100

    append file task fix betty error

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,7 +1,7 @@
 #include "main.h"
-#include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 /**
  * append_text_to_file - appends text at the end of a file
@@ -11,27 +11,27 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int fd, len, res;
+	int fd, len, res;
 
-    if (filename == NULL)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
 
-    fd = open(filename, O_WRONLY | O_APPEND);
-    if (fd == -1)
-        return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
 
-    if (text_content == NULL)
-        return (1);
+	if (text_content == NULL)
+		return (1);
 
-    len = 0;
-    while (text_content[len] != '\0')
-        len++;
+	len = 0;
+	while (text_content[len] != '\0')
+		len++;
 
-    res = write(fd, text_content, len);
-    close(fd);
+	res = write(fd, text_content, len);
+	close(fd);
 
-    if (res == -1)
-        return (-1);
+	if (res == -1)
+		return (-1);
 
-    return (1);
+	return (1);
 }
