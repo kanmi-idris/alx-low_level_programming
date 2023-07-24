e338137a54a117c9cdb98379a4dc7e9de65373b5
olaidris <olasunkanmiidris15@gmail.com>
2023-07-24 07:14:35 +0100

    fix all errors

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,10 +11,8 @@
  */
 int main(int argc, char *argv[])
 {
-	char buffer[1024];
-	int bytes_read;
-	int fd_to = open(argv[2], O_WRONLY | O_CREAT, 0664);
 	int fd_from = open(argv[1], O_RDONLY);
+	int ret = close(fd_from);
 
 	if (argc != 3)
 	{
@@ -28,17 +26,12 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	if (fd_to == -1)
+
+	if (ret == -1)
 	{
-		dprintf(2, "Error: Can't write to file %s\n", argv[2]);
-		exit(99);
+		dprintf(2, "Error: Can't close fd %d\n", fd_from);
+		exit(100);
 	}
-
-	while ((bytes_read = read(fd_from, buffer, sizeof(buffer))) > 0)
-		write(fd_to, buffer, bytes_read);
-
-	close(fd_from);
-	close(fd_to);
 
 	return (0);
 }
