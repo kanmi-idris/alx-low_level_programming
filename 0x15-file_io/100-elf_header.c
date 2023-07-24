1fdbe873c751571cba4266e201a1ba791be6d2ba
olaidris <olasunkanmiidris15@gmail.com>
2023-07-24 07:07:13 +0100

    code completed

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,42 +1,159 @@
-#include <elf.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define ELF_MAGIC_SIZE 16
+
+typedef struct
+{
+	unsigned char magic[ELF_MAGIC_SIZE];
+	unsigned char class;
+	unsigned char data;
+	unsigned char version;
+	unsigned char os_abi;
+	unsigned char abi_version;
+	unsigned char pad[7];
+	unsigned short type;
+	unsigned short machine;
+	unsigned int version2;
+	unsigned int entry_point_high;
+	unsigned int entry_point_low;
+	unsigned int ph_offset;
+	unsigned int sh_offset;
+	unsigned int flags;
+	unsigned short eh_size;
+	unsigned short ph_entry_size;
+	unsigned short ph_num;
+	unsigned short sh_entry_size;
+	unsigned short sh_num;
+	unsigned short sh_str_index;
+} ElfHeader;
+
+void print_error(const char *message)
+{
+	fprintf(stderr, "Error: %s\n", message);
+	exit(98);
+}
+
+void print_elf_header(const ElfHeader *header)
+{
+	int i;
+
+	printf("ELF Header:\n");
+	printf("  Magic:");
+	for (i = 0; i < ELF_MAGIC_SIZE; ++i)
+	{
+		printf(" %02x", header->magic[i]);
+	}
+	printf("\n");
+
+	printf("  Class:                             ");
+	switch (header->class)
+	{
+	case 1:
+		printf("ELF32\n");
+		break;
+	case 2:
+		printf("ELF64\n");
+		break;
+	default:
+		printf("<unknown: %02x>\n", header->class);
+		break;
+	}
+
+	printf("  Data:                              ");
+	switch (header->data)
+	{
+	case 1:
+		printf("2's complement, little endian\n");
+		break;
+	case 2:
+		printf("2's complement, big endian\n");
+		break;
+	default:
+		printf("<unknown: %02x>\n", header->data);
+		break;
+	}
+
+	printf("  Version:                           %u (current)\n",
+		   header->version);
+
+	printf("  OS/ABI:                            ");
+	switch (header->os_abi)
+	{
+	case 0:
+		printf("UNIX - System V\n");
+		break;
+	case 3:
+		printf("UNIX - NetBSD\n");
+		break;
+	case 6:
+		printf("UNIX - Solaris\n");
+		break;
+	default:
+		printf("<unknown: %02x>\n", header->os_abi);
+		break;
+	}
+
+	printf("  ABI Version:                       %u\n", header->abi_version);
+
+	printf("  Type:                              ");
+	switch (header->type)
+	{
+	case 1:
+		printf("REL (Relocatable file)\n");
+		break;
+	case 2:
+		printf("EXEC (Executable file)\n");
+		break;
+	case 3:
+		printf("DYN (Shared object file)\n");
+		break;
+	default:
+		printf("<unknown: %04x>\n", header->type);
+		break;
+	}
+
+	printf("  Entry point address:               0x%08x%08x\n",
+		   header->entry_point_high, header->entry_point_low);
+}
 
 int main(int argc, char *argv[])
 {
-	FILE *fp;
-	Elf32_Ehdr ehdr;
-
+	const char *filename;
+	int fd;
+	ElfHeader header;
+	unsigned char elf_magic[] = {0x7F, 'E', 'L', 'F'};
 
 	if (argc != 2)
 	{
-		fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
-		exit(1);
+		print_error("Usage: elf_header elf_filename");
 	}
 
-	fp = fopen(argv[1], "rb");
-	if (fp == NULL)
+	filename = argv[1];
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 	{
-		perror("fopen");
-		exit(1);
+		print_error("Failed to open the file");
 	}
 
-	if (fread(&ehdr, sizeof(ehdr), 1, fp) != 1)
+	if (read(fd, &header, sizeof(ElfHeader)) != sizeof(ElfHeader))
 	{
-		perror("fread");
-		exit(1);
+		close(fd);
+		print_error("Failed to read ELF header");
 	}
 
-	printf("ELF Header:\n");
-	printf("  Magic:   0x%08x\n", ehdr.e_ident[EI_MAG0]);
-	printf("  Class:                             %d\n", ehdr.e_ident[EI_CLASS]);
-	printf("  Data:                              %d\n", ehdr.e_ident[EI_DATA]);
-	printf("  Version:                           %d\n", ehdr.e_version);
-	printf("  OS/ABI:                            %d\n", ehdr.e_ident[EI_OSABI]);
-	printf("  ABI Version:                       %d\n", ehdr.e_version);
-	printf("  Type:                              %d\n", ehdr.e_type);
-	printf("  Entry point address:               0x%08x\n", ehdr.e_entry);
+	/* Verify the ELF magic number */
+	if (memcmp(header.magic, elf_magic, ELF_MAGIC_SIZE) != 0)
+	{
+		close(fd);
+		print_error("Not an ELF file");
+	}
 
-	fclose(fp);
+	print_elf_header(&header);
+
+	close(fd);
 	return 0;
 }
--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,115 +1,44 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 #include <unistd.h>
-
-#define BUFSIZE 1024
-#define USAGE "Usage: cp file_from file_to\n"
-#define READ_ERROR "Error: Can't read from file %s\n"
-#define WRITE_ERROR "Error: Can't write to %s\n"
-#define CLOSE_ERROR "Error: Can't close fd %d\n"
-
-/**
- * open_files - opens the source and destination files
- * @file_from: name of the source file
- * @file_to: name of the destination file
- * Return: an array of file descriptors, or NULL on failure
- */
-int *open_files(char *file_from, char *file_to)
-{
-	int *fds;
-	mode_t mode;
-
-	fds = malloc(sizeof(int) * 2);
-	if (fds == NULL)
-		return (NULL);
-	fds[0] = open(file_from, O_RDONLY);
-	if (fds[0] == -1)
-	{
-		dprintf(STDERR_FILENO, READ_ERROR, file_from);
-		free(fds);
-		exit(98);
-	}
-	mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
-	fds[1] = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, mode);
-	if (fds[1] == -1)
-	{
-		dprintf(STDERR_FILENO, WRITE_ERROR, file_to);
-		close(fds[0]);
-		free(fds);
-		exit(99);
-	}
-	return (fds);
-}
-
-/**
- * close_files - closes the source and destination files
- * @fds: array of file descriptors
- */
-void close_files(int *fds)
-{
-	if (close(fds[0]) == -1)
-	{
-		dprintf(STDERR_FILENO, CLOSE_ERROR, fds[0]);
-		close(fds[1]);
-		free(fds);
-		exit(100);
-	}
-	if (close(fds[1]) == -1)
-	{
-		dprintf(STDERR_FILENO, CLOSE_ERROR, fds[1]);
-		free(fds);
-		exit(100);
-	}
-	free(fds);
-}
-
-/**
- * copy_content - copies the content of a file to another file
- * @fds: array of file descriptors
- */
-void copy_content(int *fds)
-{
-	int rcount, wcount;
-	char buffer[BUFSIZE];
-
-	while ((rcount = read(fds[0], buffer, BUFSIZE)) > 0)
-	{
-		wcount = write(fds[1], buffer, rcount);
-		if (wcount != rcount)
-		{
-			dprintf(STDERR_FILENO, WRITE_ERROR, argv[2]);
-			close_files(fds);
-			exit(99);
-		}
-	}
-	if (rcount == -1)
-	{
-		dprintf(STDERR_FILENO, READ_ERROR, argv[1]);
-		close_files(fds);
-		exit(98);
-	}
-}
 
 /**
  * main - copies the content of a file to another file
- * @argc: number of arguments
- * @argv: array of arguments
- * Return: 0 on success, exit with code on failure
+ * @argc: the number of arguments
+ * @argv: the arguments
+ * Return: 0 on success
  */
 int main(int argc, char *argv[])
 {
-	int *fds;
+	char buffer[1024];
+	int bytes_read;
+	int fd_to = open(argv[2], O_WRONLY | O_CREAT, 0664);
+	int fd_from = open(argv[1], O_RDONLY);
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, USAGE);
+		dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	fds = open_files(argv[1], argv[2]);
-	copy_content(fds);
-	close_files(fds);
+
+	if (fd_from == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	if (fd_to == -1)
+	{
+		dprintf(2, "Error: Can't write to file %s\n", argv[2]);
+		exit(99);
+	}
+
+	while ((bytes_read = read(fd_from, buffer, sizeof(buffer))) > 0)
+		write(fd_to, buffer, bytes_read);
+
+	close(fd_from);
+	close(fd_to);
+
 	return (0);
 }
