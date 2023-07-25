eeef631092b857e1029964b9400cfd6e8ccfcf14
olaidris <olasunkanmiidris15@gmail.com>
2023-07-25 14:20:46 +0100

    done

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,24 +3,111 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#include <fcntl.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+/**
+ * open_files - opens the files to copy
+ *
+ * @file_from: the name of the file to copy from
+ * @file_to: the name of the file to copy to
+ * Return: an array of file descriptors, or NULL on failure
+ */
+int *open_files(char *file_from, char *file_to)
+{
+	int *fd;
+	int fd_from = open(file_from, O_RDONLY);
+	int fd_to = open(file_to, O_WRONLY | O_CREAT, 0664);
+
+	fd = malloc(sizeof(int) * 2);
+	if (fd == NULL)
+		return (NULL);
+
+	fd[0] = fd_from;
+	fd[1] = fd_to;
+
+	if (fd_from == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", file_from);
+		free(fd);
+		exit(98);
+	}
+
+	if (fd_to == -1)
+	{
+		dprintf(2, "Error: Can't write to %s\n", file_to);
+		free(fd);
+		exit(99);
+	}
+
+	return (fd);
+}
+
+/**
+ * copy_content - copies the content of a file to another file
+ *
+ * @fd: an array of file descriptors
+ */
+void copy_content(int *fd)
+{
+	char buffer[1024];
+	int bytes_read;
+	int bytes_written;
+
+	while ((bytes_read = read(fd[0], buffer, sizeof(buffer))) > 0)
+	{
+		bytes_written = write(fd[1], buffer, bytes_read);
+		if (bytes_written == -1)
+		{
+			dprintf(2, "Error: Can't write to %d\n", fd[1]);
+			free(fd);
+			exit(99);
+		}
+	}
+
+	if (bytes_read == -1)
+	{
+		dprintf(2, "Error: Can't read from file %d\n", fd[0]);
+		free(fd);
+		exit(98);
+	}
+}
+
+/**
+ * close_files - closes the files
+ *
+ * @fd: an array of file descriptors
+ */
+void close_files(int *fd)
+{
+	int res;
+
+	res = close(fd[0]);
+	if (res == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", fd[0]);
+		free(fd);
+		exit(100);
+	}
+
+	res = close(fd[1]);
+	if (res == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", fd[1]);
+		free(fd);
+		exit(100);
+	}
+
+	free(fd);
+}
 
 /**
  * main - copies the content of a file to another file
  *
  * @argc: the number of arguments
  * @argv: the arguments
- * Return: Always 0 on success
+ * Return: 0 on success and -1 on failure
  */
 int main(int argc, char *argv[])
 {
-	char buffer[1024];
-	int bytes_read;
-	int fd_to = open(argv[2], O_WRONLY | O_CREAT, 0664);
-	int fd_from = open(argv[1], O_RDONLY);
+	int *fd;
 
 	if (argc != 3)
 	{
@@ -28,38 +115,9 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 
-	if (fd_from == -1)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-
-	if (fd_to == -1)
-	{
-		dprintf(2, "Error: Can't write to file %s\n", argv[2]);
-		exit(99);
-	}
-
-	while ((bytes_read = read(fd_from, buffer, sizeof(buffer))) > 0)
-	{
-		if (write(fd_to, buffer, bytes_read) != bytes_read)
-		{
-			dprintf(2, "Error: Can't write to file %s\n", argv[2]);
-			exit(100);
-		}
-	}
-
-	if (close(fd_from) == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", fd_from);
-		exit(101);
-	}
-
-	if (close(fd_to) == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", fd_to);
-		exit(102);
-	}
+	fd = open_files(argv[1], argv[2]);
+	copy_content(fd);
+	close_files(fd);
 
 	return (0);
 }
